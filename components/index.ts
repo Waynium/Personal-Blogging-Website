b7a6ed086fe61fd91f2c4b0287c2743f90aa6caf
Wandile Nyembe <61937463+Waynium@users.noreply.github.com>
2022-07-02 10:12:51 -0700

    Added post components

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+import exp from 'constants'
+
+export { default as PostCard } from './PostCard'
+export { default as Categories } from './Categories'
+export { default as PostWidget } from './PostWidget'
+export { default as Header } from './Header'
+export { default as Layout } from './Layout'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,32 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing' },
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
-];
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return { 
+    props: { posts }
+  }
+}
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8"> {/** tailwind css classes */}
-      <Head> {/**mx => margin */}
+      <Head> {/* mx => margin */}
         <title>Wandile Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))}
+          {posts.map((post, index) => 
+            <PostCard post={post.node} key={post.title} />
+          )}
         </div>
         <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
-
+              <PostWidget />
+              <Categories />
             </div>
         </div>
       </div>
@@ -35,3 +36,4 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
