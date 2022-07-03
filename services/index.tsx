188550462c27539a2b776201435421c807d6a318
Wandile Nyembe <61937463+Waynium@users.noreply.github.com>
2022-07-03 01:43:22 -0700

    Added Comment section

--- services/index.tsx
+++ services/index.tsx
@@ -37,6 +37,41 @@ export const getPosts = async () => {
     return result.postsConnection.edges;
 }
 
+export const getPostDetails = async (slug) => {
+    const query = gql `
+        query GetPostDetails($slug: String!) {
+            post(where: { slug: $slug}) {
+                author {
+                    bio
+                    id
+                    name
+                    photo {
+                        url
+                    }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                categories {
+                    name
+                    slug
+                }
+                content {
+                    raw
+                }
+            }
+        }`
+
+    const result = await request(graphqlAPI, query, { slug });
+
+    return result.post;
+}
+
+
 export const getRecentPosts = async () => {
     const query = gql `    
         query GetPostDetails() {
@@ -58,7 +93,7 @@ export const getRecentPosts = async () => {
     return result.posts;
 }
 
-export const getSimilarPosts = async() => {
+export const getSimilarPosts = async(categories, slug) => {
     const query = gql `
         query GetPostDetails($slug: String!, $categories: [String!]) {
             posts(
@@ -75,7 +110,7 @@ export const getSimilarPosts = async() => {
             }
         }
     `
-    const result = await request(graphqlAPI, query);
+    const result = await request(graphqlAPI, query, { categories, slug });
 
     return result.posts;
 }
@@ -92,4 +127,16 @@ export const getCategories = async () => {
     const result = await request(graphqlAPI, query);
 
     return result.categories;
+}
+
+export const submitComment = async (obj) => {
+    const result = await fetch('./api/comments', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+    });
+
+    return result.json();
 }
